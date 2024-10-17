1951dc5a3793a7ceeeb854115aa6d01da58a5ef0
Shalom <youremail@gmail.com>
2024-10-17 13:33:04 +0300

    storage

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
+  loadTasks();
   const addButton = document.getElementById("add-task-btn");
   const taskInput = document.getElementById("task-input");
   const taskList = document.getElementById("task-list");
 
-  function addTask() {
+  function addTask(taskText, save = ture) {
     let taskText = taskInput.value.trim();
     if (taskText === "") {
       alert("You have entered nothing");
     } else {
       let task = document.createElement("li");
       task.textContent = taskText;
-
+      localStorage.setItem("tasks", task);
       const removeBtn = document.createElement("button");
       removeBtn.classList.add("remove-btn");
       task.appendChild(removeBtn);
@@ -18,6 +19,11 @@ document.addEventListener("DOMContentLoaded", () => {
       removeBtn.addEventListener("click", () => {
         task.remove(); // Removes the entire <li> element, including the button
       });
+    }
+    if (save) {
+      const storedTasks = JSON.parse(localStorage.getItem("taks") || []);
+      storedTasks.push(taskText);
+      localStorage.setItem("taks", JSON.stringify(storedTasks));
     }
   } //addTask is used to add tasks to the page
   addButton.addEventListener("click", addTask);
@@ -28,4 +34,8 @@ document.addEventListener("DOMContentLoaded", () => {
       addTask();
     }
   });
+  function loadTasks() {
+    const storedTasks = JSON.parse(localStorage.getItem("taks") || "[]");
+    storedTasks.forEach((taskText) => addTask(taskText, false));
+  }
 });
